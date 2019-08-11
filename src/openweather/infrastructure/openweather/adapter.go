f1af3ccfd631e78aa3c32e095c44a73f9d0d9aa1
Thorsten Essig <thorsten.essig@aoe.com>
2019-08-11 17:07:19 +0200

    step 4

--- src/openweather/infrastructure/openweather/adapter.go
+++ src/openweather/infrastructure/openweather/adapter.go
@@ -2,7 +2,10 @@ package openweather
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"net/http"
 
 	"flamingo.me/example-openweather/src/openweather/domain"
 )
@@ -14,7 +17,44 @@ type (
 	}
 
 	weatherDto struct {
-		// use https://mholt.github.io/json-to-go/ to generate it easily
+		Coord struct {
+			Lon float64 `json:"lon"`
+			Lat float64 `json:"lat"`
+		} `json:"coord"`
+		Weather []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Base string `json:"base"`
+		Main struct {
+			Temp     float64 `json:"temp"`
+			Pressure int     `json:"pressure"`
+			Humidity int     `json:"humidity"`
+			TempMin  float64 `json:"temp_min"`
+			TempMax  float64 `json:"temp_max"`
+		} `json:"main"`
+		Visibility int `json:"visibility"`
+		Wind       struct {
+			Speed float64 `json:"speed"`
+			Deg   float64 `json:"deg"`
+		} `json:"wind"`
+		Clouds struct {
+			All int `json:"all"`
+		} `json:"clouds"`
+		Dt  int `json:"dt"`
+		Sys struct {
+			Type    int     `json:"type"`
+			ID      int     `json:"id"`
+			Message float64 `json:"message"`
+			Country string  `json:"country"`
+			Sunrise int     `json:"sunrise"`
+			Sunset  int     `json:"sunset"`
+		} `json:"sys"`
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+		Cod  int    `json:"cod"`
 	}
 )
 
@@ -36,9 +76,38 @@ func (a *Adapter) Inject(
 
 // GetByCity returns the weather for the given city
 func (a *Adapter) GetByCity(ctx context.Context, city string) (domain.Weather, error) {
-	return domain.Weather{}, ErrNoWeather
+	resp, err := a.apiClient.request(ctx, http.MethodGet, "weather?q="+city, nil)
+	if err != nil {
+		return domain.Weather{}, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return domain.Weather{}, err
+	}
+
+	dto := new(weatherDto)
+	err = json.Unmarshal(body, dto)
+	if err != nil {
+		return domain.Weather{}, err
+	}
+	return mapDto(dto)
 }
 
 func mapDto(dto *weatherDto) (domain.Weather, error) {
-	return domain.Weather{}, nil
+	if len(dto.Weather) < 1 {
+		return domain.Weather{}, ErrNoWeather
+	}
+	return domain.Weather{
+		MainCharacter: dto.Weather[0].Main,
+		Description:   dto.Weather[0].Description,
+		IconCode:      dto.Weather[0].Icon,
+		Temp:          int(dto.Main.Temp),
+		Humidity:      dto.Main.Humidity,
+		TempMin:       int(dto.Main.TempMin),
+		TempMax:       int(dto.Main.TempMax),
+		WindSpeed:     dto.Wind.Speed,
+		Cloudiness:    dto.Clouds.All,
+		LocationName:  dto.Name,
+	}, nil
 }
--- src/openweather/module.go
+++ src/openweather/module.go
@@ -5,7 +5,7 @@ import (
 	"flamingo.me/flamingo/v3/framework/web"
 
 	"flamingo.me/example-openweather/src/openweather/domain"
-	"flamingo.me/example-openweather/src/openweather/infrastructure"
+	"flamingo.me/example-openweather/src/openweather/infrastructure/openweather"
 	"flamingo.me/example-openweather/src/openweather/interfaces"
 )
 
@@ -16,7 +16,8 @@ type Module struct{}
 func (m *Module) Configure(injector *dingo.Injector) {
 	web.BindRoutes(injector, new(routes))
 
-	injector.Bind(new(domain.Service)).To(infrastructure.Fakeservice{})
+	injector.Bind(new(domain.Service)).To(openweather.Adapter{})
+	// injector.Bind(new(domain.Service)).To(infrastructure.Fakeservice{})
 }
 
 type routes struct {
